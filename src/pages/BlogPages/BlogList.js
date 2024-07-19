864eef93c48cac06411b07e14c4528fff4a6a1ca
arshiyagarg <arshiyagarg932@gmail.com>
2024-07-19 16:41:01 +0530

    Added NSE link

--- src/pages/BlogPages/BlogList.js
+++ src/pages/BlogPages/BlogList.js
@@ -1,9 +1,9 @@
 const blogs=[
     {
-        name:"Smart Hackathon (Ferozpur)",
-        author:"Aastha Singhal",
-        category:"hackathons",
-        content:"Participating in the Smart Hackathon organized by Shaheed Bhagat Singh State University in Ferozpur, Punjab was an unforgettable experience that pushed the boundaries of our abilities and expanded our horizons. This 36-hour-long hackathon marked our first foray into the world of offline hackathons, and it was a rollercoaster ride of challenges, innovation, and personal growth.",
+        name:"Check Out Real Time Prices at NSE",
+        author:"NSE",
+        category:"Stock Price Indian Market",
+        content:"",
         image:"https://res.cloudinary.com/duxmh9dws/image/upload/v1694970533/Screenshot_2023-09-17_at_10.24.29_PM_scnmhq.png"
     },
     {
