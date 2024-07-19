1b5835c4f33f0732a13b59d0e4efe80f68164550
Aditi Mehta <141945188+aditi75432@users.noreply.github.com>
2024-07-19 15:18:33 +0530

    updated images directory - Aditi Mehta

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -8,10 +8,8 @@ function About() {
         data-aos="fade-left"
         data-aos-duration="500"
       >
-        <h4>
-          We are here, to be the perfect guidance you need to ace the
-          engineering journey you are on!
-        </h4>
+        <h4>loremmmmm</h4>
+        
         We are the MICROSOFT LEARN STUDENT AMBASSADOR STUDENT CHAPTER, your one
         stop spot for all the amazing sessions and events. This is where you can
         grow by learning from the mistakes, experiences, success and failure of
@@ -20,7 +18,7 @@ function About() {
         stuff we have in store for you all!
       </div>
       <img
-        src="/public/images/home_background.jpg"
+        src="./images/home_background.jpg"
         className="imagesection"
         data-aos="fade-right"
         data-aos-duration="500"
