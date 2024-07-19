eb1fc02303dad747d052ab2050f84c36f9cb3fc5
Aditi Mehta <141945188+aditi75432@users.noreply.github.com>
2024-07-19 16:58:11 +0530

    updated content blog

--- src/pages/BlogPages/BlogList.js
+++ src/pages/BlogPages/BlogList.js
@@ -3,7 +3,7 @@ const blogs=[
         name:"Check Out Real Time Prices at NSE",
         author:"NSE",
         category:"Stock Price Indian Market",
-        content:"",
+        content:"Learn more about stck market",
         image:"https://res.cloudinary.com/duxmh9dws/image/upload/v1694970533/Screenshot_2023-09-17_at_10.24.29_PM_scnmhq.png"
     },
     {
